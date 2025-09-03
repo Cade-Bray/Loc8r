5a54686d74267d07a5195a0d5de1799a8cbbaa91
Cade-Bray <bray.cade@gmail.com>
2025-09-03 11:38:16 -0600

    Seperated the controller and the route

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
-});
+const homepageController = (req, res) => {
+  res.render('index', {title: 'Express'});
+};
+router.get('/', homepageController);
 
 module.exports = router;
