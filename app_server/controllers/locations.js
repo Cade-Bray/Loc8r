a642d4462ce17a6a8e896c98d283b837ce7ca051
Cade-Bray <bray.cade@gmail.com>
2025-09-15 17:08:32 -0600

    Add generic text page and location review form with dynamic rendering.

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,6 +1,35 @@
 /* GET 'home' page */
 const homelist = (req, res) => {
-    res.render('locations-list', {title: 'Home'});
+    res.render('locations-list', {
+        title: 'Home',
+        pageHeader: {
+            title: 'Loc8r',
+            strapline: 'Find places to work with wifi near you!'
+        },
+        locations: [
+            {
+                name: 'Starcups',
+                address: '125 High Street, Reading, RG6 1PS',
+                rating: 3,
+                facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+                distance: '100m'
+            },
+            {
+                name: 'Cafe Hero',
+                address: '125 High Street, Reading, RG6 1PS',
+                rating: 4,
+                facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+                distance: '200m'
+            },
+            {
+                name: 'Burger Queen',
+                address: '125 High Street, Reading, RG6 1PS',
+                rating: 2,
+                facilities: ['Food', 'Premium wifi'],
+                distance: '250m'
+            }
+        ]
+    });
 };
 
 /* GET 'Location info' page */
@@ -10,7 +39,7 @@ const locationInfo = (req, res) => {
 
 /* GET 'Add review' page */
 const addReview = (req, res) => {
-    res.render('index', {title: 'Add review'});
+    res.render('location-review-form', {title: 'Add review'});
 };
 
 module.exports = {
